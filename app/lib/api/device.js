4541a8396e8314c396fd3f49cbce731c19c72394
이동현(DongHyun LEE) <ldh@covision.co.kr>
2021-04-23 10:52:00 +0900

    device 추가 nf 처리 / 1.9.2 update

--- app/lib/api/device.js
+++ app/lib/api/device.js
@@ -1,5 +1,5 @@
 import { managersvr } from '@API/api';
 
 export const addDevice = params => {
-  return managersvr('post', '/na/device/set.do', params);
+  return managersvr('post', '/na/nf/device/set.do', params);
 };
