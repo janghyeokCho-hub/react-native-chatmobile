dfe54b057c799b9314f17b3e928742c09c96a1c4
Kyoungjun Min <kjmin@covision.co.kr>
2021-07-15 07:18:12 +0000

    Resolve "쪽지알림 처리"

--- app/components/PushContainer.js
+++ app/components/PushContainer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Platform } from 'react-native';
+import React, { useEffect, useCallback } from 'react';
+import { Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import DeviceInfo from 'react-native-device-info';
 import messaging from '@react-native-firebase/messaging';
@@ -10,7 +10,6 @@ import { openRoom } from '@/modules/room';
 import { openChannel } from '@/modules/channel';
 import { moveToRoom } from '@/lib/roomUtil';
 import { moveToChannelRoom } from '@/lib/channelUtil';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const PushContainer = ({ navigator }) => {
   const { userInfo } = useSelector(({ login }) => ({
@@ -29,6 +28,41 @@ const PushContainer = ({ navigator }) => {
 
   useEffect(() => {
     if (userInfo) {
+      const handleNotification = (notification, completion) => {
+        if (typeof notification === 'undefined') {
+          // 정상적인 알림이 아닐 경우 동작하지 않도록 함
+          return;
+        }
+        if (notification?.noteId || notification?.payload?.noteId) {
+          // 쪽지알림 처리
+          Alert.alert('쪽지', '쪽지는 PC에서 학인해주세요.');
+          return;
+        }
+
+        const { roomID } = notification.roomID ? notification : notification?.payload;
+        const { roomType } = notification?.roomType ? notification : notification?.payload;
+
+        // 방 정보가 유효하지 않을 경우 동작하지 않도록 함
+        if (!roomID || !roomType || notification?.data) {
+          return;
+        }
+        if (roomType === 'C') {
+          dispatch(openChannel({ roomID: roomID }));
+          moveToChannelRoom(navigator, 'ChannelRoom', {
+            roomID,
+          });
+        } else if (roomType === 'M') {
+          dispatch(openRoom({ roomID: roomID }));
+          moveToRoom(navigator, 'ChatRoom', {
+            roomID,
+          });
+        }
+
+        // Notifications.events().registerNotificationOpened
+        if (typeof completion !== 'undefined') {
+          completion();
+        }
+      };
       requestPermission().then(result => {
         if (result) {
           requestToken().then(token => {
@@ -43,104 +77,17 @@ const PushContainer = ({ navigator }) => {
 
             PushNotification.setApplicationIconBadgeNumber(0);
 
-            PushNotification.configure({
-              onNotification: notification => {
-                // Android Push Notification Open Event Linstener(Background)
-                if (notification != undefined) {
-                  if (navigator != undefined) {
-                    const { roomID } = notification.roomID
-                      ? notification
-                      : notification.payload;
-                    const { roomType } = notification.roomType
-                      ? notification
-                      : notification.payload && notification.payload.roomType
-                      ? notification.payload
-                      : { roomType: null };
-
-                    if (!notification.data) {
-                      if (roomType == 'C') {
-                        dispatch(openChannel({ roomID: roomID }));
-                        moveToChannelRoom(navigator, 'ChannelRoom', {
-                          roomID: roomID,
-                        });
-                      } else {
-                        dispatch(openRoom({ roomID: roomID }));
-                        moveToRoom(navigator, 'ChatRoom', {
-                          roomID: roomID,
-                        });
-                      }
-                    }
-                  }
-                }
-                if (Platform.OS === 'ios')
-                  notification.finish(PushNotificationIOS.FetchResult.NoData);
-              },
-            });
-
             Notifications.events().registerNotificationReceivedBackground(
               (notification, completion) => {
                 completion({ alert: true, sound: true, badge: false });
               },
             );
-            // 안드로이드에서 실행 방지('유효하지 않은 접근' 에러 방어)
-            Notifications.events().registerNotificationOpened(
-              (notification, completion) => {
-                // iOS Push NOtification Open Event Linstener(Background)
-                if (notification !== undefined) {
-                  if (navigator !== undefined) {
-                    // notification.room* undefined 에러 방어
-                    const { roomID } = notification.roomID
-                      ? notification
-                      : notification.payload;
-                    const { roomType } = notification.roomType
-                      ? notification
-                      : notification.payload && notification.payload.roomType
-                      ? notification.payload
-                      : { roomType: null };
-                    if (roomType === 'C') {
-                      dispatch(openChannel({ roomID }));
-                      moveToChannelRoom(navigator, 'ChannelRoom', {
-                        roomID,
-                      });
-                    } else {
-                      dispatch(openRoom({ roomID }));
-                      moveToRoom(navigator, 'ChatRoom', {
-                        roomID,
-                      });
-                    }
-                  }
-                }
-                completion();
-              },
-            );
-            Notifications.getInitialNotification()
-              .then(notification => {
-                // Android & iOS Push Notification Open Event Linstener(Background and App is close)
-                if (notification != undefined) {
-                  if (navigator != undefined) {
-                    const { roomID } = notification.roomID
-                      ? notification
-                      : notification.payload;
-                    const { roomType } = notification.roomType
-                      ? notification
-                      : notification.payload && notification.payload.roomType
-                      ? notification.payload
-                      : { roomType: null };
 
-                    if (roomType == 'C') {
-                      dispatch(openChannel({ roomID: roomID }));
-                      moveToChannelRoom(navigator, 'ChannelRoom', {
-                        roomID: roomID,
-                      });
-                    } else {
-                      dispatch(openRoom({ roomID: roomID }));
-                      moveToRoom(navigator, 'ChatRoom', {
-                        roomID: roomID,
-                      });
-                    }
-                  }
-                }
-              })
+            /* iOS Push Notification Open Event Linstener(Background) */
+            Notifications.events().registerNotificationOpened(handleNotification);
+
+            /* Android & iOS Push Notification Open Event Linstener(Background and App is close) */
+            Notifications.getInitialNotification().then(handleNotification)
               .catch(error => {
                 console.log(error);
               });
