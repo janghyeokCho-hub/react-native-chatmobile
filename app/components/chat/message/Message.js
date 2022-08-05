bf9ba43652b488dc32a58483f0319da9af7c4bda
Kyoungjun Min <kjmin@covision.co.kr>
2022-08-05 13:12:20 +0900

    issue 111 원복

--- app/components/chat/message/Message.js
+++ app/components/chat/message/Message.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Plain, Link, Tag, Sticker, Mention } from '@C/chat/message/types';
 import { View, StyleSheet } from 'react-native';
 import { useTheme } from '@react-navigation/native';
-import { convertChildren } from '@/lib/messageUtil';
+import { getAttribute } from '@/lib/messageUtil';
 
 const Message = ({
   children,
@@ -14,6 +15,126 @@ const Message = ({
   longPressEvt,
 }) => {
   const { colors, sizes } = useTheme();
+  const [drawText, setDrawText] = useState(<View />);
+
+  useEffect(() => {
+    const pattern = new RegExp(
+      /[<](LINK|NEWLINE|TAG|STICKER|MENTION|MOVE)[^>]*[/>]/,
+      'gi',
+    );
+
+    let newLineJSX = [];
+    let returnJSX = [];
+    let beforeLastIndex = 0;
+    let match = null;
+
+    while ((match = pattern.exec(children)) != null) {
+      if (match.index > 0 && match.index > beforeLastIndex) {
+        returnJSX.push(
+          <Plain
+            key={returnJSX.length}
+            text={children.substring(beforeLastIndex, match.index)}
+            marking={marking}
+            style={{ ...styles[styleType], fontSize: sizes.chat }}
+            longPressEvt={longPressEvt}
+          />,
+        );
+      }
+
+      const attrs = getAttribute(match[0]);
+
+      if (match[1] === 'LINK') {
+        returnJSX.push(
+          <Link
+            key={returnJSX.length}
+            marking={marking}
+            style={{ ...styles[styleType], fontSize: sizes.chat }}
+            longPressEvt={longPressEvt}
+            {...attrs}
+          />,
+        );
+      } else if (match[1] === 'NEWLINE') {
+        if (returnJSX.length === 0) {
+          newLineJSX.push(
+            <View key={newLineJSX.length} style={styles.lineBreaker}>
+              <Plain
+                key="newline_0"
+                text=""
+                marking={marking}
+                style={{ ...styles[styleType], fontSize: sizes.chat }}
+              />
+            </View>,
+          );
+        } else {
+          newLineJSX.push(
+            <View key={newLineJSX.length} style={styles.lineBreaker}>
+              {[...returnJSX]}
+            </View>,
+          );
+
+          returnJSX = [];
+        }
+      } else if (match[1] === 'TAG') {
+        returnJSX.push(
+          <Tag
+            key={returnJSX.length}
+            marking={marking}
+            style={{ ...styles[styleType], fontSize: sizes.chat }}
+            longPressEvt={longPressEvt}
+            {...attrs}
+          />,
+        );
+      } else if (match[1] === 'STICKER') {
+        returnJSX.push(<Sticker key={returnJSX.length} {...attrs} />);
+      } else if (match[1] === 'MENTION') {
+        returnJSX.push(
+          roomInfo && (
+            <Mention
+              key={returnJSX.length}
+              marking={marking}
+              mentionInfo={roomInfo.members}
+              style={{ ...styles.sentMentionText, fontSize: sizes.chat }}
+              navigation={navigation}
+              longPressEvt={longPressEvt}
+              {...attrs}
+            />
+          ),
+        );
+      } else if (match[1] === 'MOVE') {
+        returnJSX.push(
+          <Plain
+            key={returnJSX.length}
+            text={'?'}
+            marking={marking}
+            style={{ ...styles[styleType], fontSize: sizes.chat }}
+            longPressEvt={longPressEvt}
+          />,
+        );
+      } else {
+      }
+      beforeLastIndex = match.index + match[0].length;
+    }
+
+    if (beforeLastIndex < children.length)
+      returnJSX.push(
+        <Plain
+          key={returnJSX.length}
+          text={children.substr(beforeLastIndex)}
+          marking={marking}
+          style={{ ...styles[styleType], fontSize: sizes.chat }}
+          longPressEvt={longPressEvt}
+        />,
+      );
+
+    if (returnJSX.length > 0) {
+      newLineJSX.push(
+        <View key={newLineJSX.length} style={styles.lineBreaker}>
+          {[...returnJSX]}
+        </View>,
+      );
+    }
+    setDrawText(newLineJSX);
+  }, [children]);
 
   return (
     <>
@@ -29,16 +150,7 @@ const Message = ({
           ]}
           id={eleId}
         >
-          {convertChildren({
-            children,
-            style,
-            styleType,
-            marking,
-            roomInfo,
-            sizes,
-            longPressEvt,
-            navigation,
-          })}
+          {drawText}
         </View>
       )) || (
         <View
@@ -51,16 +163,7 @@ const Message = ({
             styles.container,
           ]}
         >
-          {convertChildren({
-            children,
-            style,
-            styleType,
-            marking,
-            roomInfo,
-            sizes,
-            longPressEvt,
-            navigation,
-          })}
+          {drawText}
         </View>
       )}
     </>
@@ -71,6 +174,13 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     alignItems: 'flex-start',
+  },
+  lineBreaker: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
   },
   repliseText: {
     color: '#fff',
