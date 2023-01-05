bc6b76e2a7ea229855ae04227f4eb13cf3927f0e
jhcho <janghyeok9257@gmail.com>
2023-01-05 11:47:29 +0900

    IOS RCTLinking link

--- ios/chatmobile/AppDelegate.m
+++ ios/chatmobile/AppDelegate.m
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+#import <React/RCTLinkingManager.h> 
 #import "AppDelegate.h"
 
 #import "RNNotifications.h"
@@ -93,4 +93,10 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
   return [Orientation getOrientation];
 }
 
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 @end
